f145fb56898343de9260fc13b458dab2fbe6ab13
roma2k <roma2k@gmail.com>
2021-04-03 18:03:57 +0700

    right ids2hash

--- src/Components/RomList/createActionLink.js
+++ src/Components/RomList/createActionLink.js
@@ -1,4 +1,36 @@
-function ids2hash(ids=[]) {
+const ids2hash = ids => {
+    if (ids.length === 0) return false;
+    // Получаем максимальное значение
+    let sorted = [...ids];
+    sorted = sorted.sort((x, y) => x -  y);
+    let max_id = sorted[sorted.length - 1];
+    let hash = "";
+    let curhash = 0;
+    // Перебираем все возможные ID
+    for(let i = 0; i <= max_id; i++) {
+        if (sorted.includes(i)) {
+            // Если ID игры есть в списке, устанавливаем её бит
+            curhash |= 1 << (i % 8);
+        }
+        if (((i % 8) === 7) || (i === max_id)) {
+            // костыль для преобразования curhash в hex строго длины 2
+            hash = hash + (curhash+0x100).toString(16).substr(-2).toUpperCase();
+            curhash = 0;
+        }
+    }
+    // В резльтате получается очень много нулей, группируем их символами, которые отсутствуют в шестнадцатеричном наборе
+    hash = hash.replaceAll("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", "T");
+    hash = hash.replaceAll("0000000000000000000000000000000000000000000000000000000000000000", "U");
+    hash = hash.replaceAll("00000000000000000000000000000000", "V");
+    hash = hash.replaceAll("0000000000000000", "W");
+    hash = hash.replaceAll("00000000", "X");
+    hash = hash.replaceAll("0000", "Y");
+    hash = hash.replaceAll("00", "Z");
+    return hash;
+
+}
+
+/*function ids2hash1(ids=[]) {
     if (ids.length == 0) return "";
     // Получаем максимальное значение
     ids = ids.sort((x, y) => x -  y);
@@ -26,8 +58,6 @@ function ids2hash(ids=[]) {
     hash = hash.replaceAll("0000", "Y");
     hash = hash.replaceAll("00", "Z");
     return hash;
-}
-
-console.log(ids2hash([1,5,100,153,6743]))
+}*/
 
 export default ids2hash;
